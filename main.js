e23ba7d6e71917216ebea00411f0f2148c7e3d8a
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2021-10-01 12:56:26 +0200

    Fetch modified
    Function fetch simplified and comments added

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 
 fetch("./FishEyeData.json")
-.then(response => {
-    return response.json()
-})
-.then(function profilPhotographes(jsonObj) { 
-    let photographes = jsonObj["photographers"]; 
+.then(response => response.json()) // fonction fléchée; fonction standard aurait été écrit comme suit: .then(function(response){return response.json()})
+.then(function profilPhotographes(jsonObj) { // jsonObj est l'objet retourné par le .then précedent
+    let photographes = jsonObj["photographers"]; // on accède à l'objet photographers dans l'objet jsonObj
 
     photographes.forEach(photographe => {
         let html = document.createElement("div");
 
-        html.innerHTML = '<div class="name">' + photographe["name"] + '</div><span class="city">' + photographe["city"] + '</span><span class="country">' + photographe["country"] + '</span><div class="tagline">' + photographe["tagline"] + '</div><div class="price">' + photographe["price"] + '</div><div class="tags">' + photographe["tags"] + '</div>';
-        
+        html.innerHTML = `<div class="name"> ${photographe["name"]} </div><span class="city"> ${photographe["city"]}, </span><span class="country"> ${photographe["country"]} </span><div class="tagline"> ${photographe["tagline"]} </div><div class="price"> ${photographe["price"]}€/jour </div><div class="tags"> ${photographe["tags"]} </div>`;
+
         document.querySelector("section").appendChild(html);
     });
 });

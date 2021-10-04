78d76e1441e1107886f550396281534f0be7c847
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2021-10-04 23:07:38 +0200

    Static Html AND dynamic JS for homepage (with all styling)
    Html:
    First I created a static version of Html (to play with CSS and be able to create the page in accordance with  provided visual model). Only then I created the same content via JS by mirroring my Html
    (I will remove the redundant Html from index.html in the next commit)
    
    CSS:
    All styling done
    
    JS:
    - .innerHTML modified in order to mirror the expected Html structure
    - images added via .innerHTML
    - loop added to create separate <li>'s for each tag (in JSON all tags are within one key)
    - function showAllPhotographers created and called inside the fetch -> a different way to manage the fetch function

--- main.js
+++ main.js
@@ -1,14 +1,53 @@
+let jsonPhotoDb;
+
+function showAllPhotographers(photoDb) {
+    let photographes = photoDb["photographers"]; // on accède à l'objet photographers dans l'objet jsonObj
+
+    photographes.forEach(ph => {
+        let html = document.createElement("div");
+        html.classList.add("card");
+
+        let liTags = '';
+
+        for (let tag of ph['tags']) { // cette méthode de for peut être utilisée pour les objets de type array
+            liTags += `<li>#${tag}</li>`; // façon standard de for serait êcrit comme suit: 
+        }
+
+        // for (let i=0; i < ph['tags'].length; i++) { 
+        //     liTags += `<li>#${ph['tags'][i]}</li>`;
+        // }
+
+        html.innerHTML = `<div><a href="#">
+                          <img src="photos/Photographers_ID_Photos/${ph["portrait"]}" alt="portrait_photographe">
+                          </a></div>
+                          <h2> ${ph["name"]} </h2>
+                          <p class="location"> ${ph["city"]}, ${ph["country"]} </p>
+                          <p class="motto"> ${ph["tagline"]} </p>
+                          <p class="price"> ${ph["price"]}€/jour </p>
+                          <ul> ${liTags} </ul>`;
+
+        document.querySelector("section").appendChild(html);
+    });
+
+}
 
 fetch("./FishEyeData.json")
 .then(response => response.json()) // fonction fléchée; fonction standard aurait été écrit comme suit: .then(function(response){return response.json()})
 .then(function profilPhotographes(jsonObj) { // jsonObj est l'objet retourné par le .then précedent
+    jsonPhotoDb = jsonObj;
+
+    showAllPhotographers(jsonPhotoDb);
+/*    
     let photographes = jsonObj["photographers"]; // on accède à l'objet photographers dans l'objet jsonObj
 
     photographes.forEach(photographe => {
         let html = document.createElement("div");
+        html.classList.add("card");
 
-        html.innerHTML = `<div class="name"> ${photographe["name"]} </div><span class="city"> ${photographe["city"]}, </span><span class="country"> ${photographe["country"]} </span><div class="tagline"> ${photographe["tagline"]} </div><div class="price"> ${photographe["price"]}€/jour </div><div class="tags"> ${photographe["tags"]} </div>`;
+        html.innerHTML = `<h2> ${photographe["name"]} </h2><p class="location"> ${photographe["city"]}, ${photographe["country"]} </p><p class="motto"> ${photographe["tagline"]} </p><p class="price"> ${photographe["price"]}€/jour </p><ul> ${photographe["tags"]} </ul>`;
 
         document.querySelector("section").appendChild(html);
     });
+*/
 });
+
